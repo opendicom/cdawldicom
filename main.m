32970f2dc3bc649585ad434315fdd5c0d0ee8ad4
jacquesfauquex <jacquesfauquex@gmail.com>
2021-02-12 07:49:46 -0300

    improved filesystem

--- main.m
+++ main.m
@@ -25,7 +25,7 @@ int main(int argc, const char * argv[])
      
 #pragma mark init
      
-    NSError *err=nil;
+    NSError *error=nil;
     const uint64 tag00420011=0x0000424F00110042;//encapsulatedCDA tag + vr + padding (used in order to find the offset)
     NSFileManager *fileManager=[NSFileManager defaultManager];
 
@@ -41,152 +41,208 @@ int main(int argc, const char * argv[])
      //[1] xslt1 path
      //[2] qido base url
      //[3] audit folder
-     
+     //[4] scpaet
+
      
      
      //[1]cda2mwl.xsl path
      NSData *xslt1Data=[NSData dataWithContentsOfFile:[args[1] stringByExpandingTildeInPath]];
      
-    //NSString *DA=@"20161018";
-    NSString *DA=[DAFormatter stringFromDate:[NSDate date]];
-     
+     //NSString *DA=@"20161018";
+     NSString *DA=[DAFormatter stringFromDate:[NSDate date]];
+     NSDictionary *DAargdict = [NSDictionary dictionaryWithObject:DA forKey:@"now"];
+
      //arg[2] base URL
      //ejemplo: https://serviciosridi.asse.uy/dcm4chee-arc/qido/DCM4CHEE/instances?Modality=OT&SeriesDescription=solicitud&NumberOfStudyRelatedInstances=1&00080080=asseMALDONADO&StudyDate=20210128&StudyTime=1210-
      NSURL *qidoURL=[NSURL URLWithString:[NSString stringWithFormat:@"%@%@",args[2],DA]];
      
      //arg[3] audit path
-     NSString *todayAuditPath=[[args[3] stringByExpandingTildeInPath] stringByAppendingPathComponent:DA];
+     NSString *DApath=[[args[3] stringByExpandingTildeInPath] stringByAppendingPathComponent:DA];
      
      
 #pragma mark qido
      NSData *qidoResponse=[NSData dataWithContentsOfURL:qidoURL
                                                 options:NSDataReadingUncached
-                                                  error:&err];
-     if (err)
+                                                  error:&error];
+     if (error)
      {
-        LOG_ERROR(@"%@",[err description]);
-        exit(0)
+        LOG_ERROR(@"%@",[error description]);
+         exit(0);
      }
      if (!qidoResponse)
      {
         LOG_WARNING(@"no answer to qido");
-        exit(0)
+         exit(0);
      }
      if (![qidoResponse length]) exit(0);
 
 
-      NSArray *list = [NSJSONSerialization JSONObjectWithData:qidoResponse options:0 error:&err];
-      //NSLog(@"%@",[list description]);
+      NSArray *list = [NSJSONSerialization JSONObjectWithData:qidoResponse options:0 error:&error];
+      if (!list)
+      {
+          LOG_ERROR(@"qido response not json. %@ %@",[error description],[[NSString alloc]initWithData:qidoResponse encoding:NSUTF8StringEncoding] );
+          exit(1);
+      }
+     
+     
       for (NSDictionary *instance in list)
       {
-#pragma mark loop for each solicitud
-          NSDictionary *now = [NSDictionary dictionaryWithObject:DA forKey:@"now"];
+#pragma mark - loop StudyInstanceUIDs
           
-          NSString *PatientID=[[[instance objectForKey:@"00100020"]objectForKey:@"Value"]firstObject];
-          NSString *StudyInstanceUID=[[[instance objectForKey:@"0020000D"]objectForKey:@"Value"]firstObject];
-          NSURL *RetrieveURL=[NSURL URLWithString:[[[instance objectForKey:@"00081190"]objectForKey:@"Value"]firstObject]];//RetrieveURL
-          NSString *sopAuditPath=[[todayAuditPath stringByAppendingPathComponent:PatientID] stringByAppendingPathComponent:StudyInstanceUID];
-
-         
-          //if already downloaded
-          if([fileManager fileExistsAtPath:sopAuditPath])continue;
-
-         
+          NSString *EUID=[[[instance objectForKey:@"0020000D"]objectForKey:@"Value"]firstObject];
+          NSString *EUIDpath=[DApath stringByAppendingPathComponent:EUID];
           
-          LOG_INFO(@"wado-rs solicitud: %@",sopAuditPath);
-          if(![fileManager createDirectoryAtPath:sopAuditPath withIntermediateDirectories:YES attributes:nil error:nil])
-          {
-             LOG_ERROR(@"ERROR could not create folder");
-             continue;
-          }
-         
-         
-          NSData *downloaded=[NSData dataWithContentsOfURL:RetrieveURL];
-           if (!downloaded)
-          {
-            LOG_WARNING(@"no answer to downloaded");
-            continue;
-          }
-          if (![qidoResponse length]) exit(0);
+          if([fileManager fileExistsAtPath:[EUIDpath stringByAppendingPathComponent:@"wi.xml"]]) continue;//WorkItem (wi.xml) already downloaded
           
-          //unzip
-          NSError *error=nil;
-          ZZArchive *archive = [ZZArchive archiveWithData:downloaded];
-          ZZArchiveEntry *firstEntry = archive.entries[0];
-          NSData *unzipped = [firstEntry newDataWithError:&error];
-          if (error!=nil)
+          if(![fileManager fileExistsAtPath:EUIDpath] && ![fileManager createDirectoryAtPath:EUIDpath withIntermediateDirectories:YES attributes:nil error:nil])
           {
-             LOG_WARNING(@"ERROR could NOT unzip");
-             [downloaded writeToFile:[sopAuditPath stringByAppendingPathComponent:@"unzippeable.zip"] atomically:NO];
-             continue;
-          }
-         
-          //get CDA: 00420011
-          NSRange range00420011=[unzipped rangeOfData:[NSData dataWithBytes:(void*)&tag00420011 length:8]
-                                               options:0
-                                                 range:NSMakeRange(0,[unzipped length])];
-          if (range00420011.location==NSNotFound)
-          {
-              LOG_WARNING(@"no contiene attr 00420011");
-             [unzipped writeToFile:[sopAuditPath stringByAppendingPathComponent:@"noTag00420011.xml"] atomically:NO];
+              LOG_ERROR(@"could not create folder %@", EUIDpath);
               continue;
           }
-          //get CDA: 00420011.length
-          uint32 capsuleLength=0x00000000;
-          [unzipped getBytes:&capsuleLength range:NSMakeRange(range00420011.location+8,4)];
-            
-          unsigned char padded=0xFF;
-          [unzipped getBytes:&padded range:NSMakeRange(range00420011.location+12+capsuleLength-1,1)];
-          NSData *encapsulatedData=[[NSData alloc]initWithData:[unzipped subdataWithRange:NSMakeRange(range00420011.location+12,capsuleLength-(padded==0))]];
-            
-          if (!encapsulatedData)
+          else
           {
-            LOG_WARNING(@"attr 00420011 empty");
-            continue;
-          }
+#pragma mark AccessionNumber
+              //        /AccessionNumber[^[localIssuer][^universalIssuer]]
+              //                /symlink StudyInstanceUID -> StudyInstanceUID
+              
+              NSString *ANIsuffix=nil;
+              NSString *ANL=[[[instance objectForKey:@"00080051.00400031"]objectForKey:@"Value"]firstObject];
+              NSString *ANU=[[[instance objectForKey:@"00080051.00400032"]objectForKey:@"Value"]firstObject];
+              if (ANL && ANU) ANIsuffix=[NSString stringWithFormat:@"^%@^%@",ANL,ANU];
+              else if (ANL)   ANIsuffix=[NSString stringWithFormat:@"^%@",ANL];
+              else if (ANU)   ANIsuffix=[NSString stringWithFormat:@"^%@",ANU];
+              else            ANIsuffix=@"";
+              NSString *AN=[[[[instance objectForKey:@"00080050"]objectForKey:@"Value"]firstObject]stringByAppendingString:ANIsuffix];
+              NSString *ANpath=[DApath stringByAppendingPathComponent:AN];
+              if(![fileManager fileExistsAtPath:ANpath])
+              {
+                  if(![fileManager createDirectoryAtPath:ANpath withIntermediateDirectories:YES attributes:nil error:nil])
+                  {
+                      LOG_ERROR(@"could not create folder %@", ANpath);
+                      continue;
+                  }
+              }
+              if (![fileManager createSymbolicLinkAtPath:[ANpath stringByAppendingPathComponent:EUID] withDestinationPath:EUIDpath error:&error])
+              {
+                  LOG_ERROR(@"could not create AccessionNumber symlink %@ to %@. %@",AN, EUIDpath, [error description]);
+                  continue;
+              }
 
-          NSXMLDocument *xmlDocument = [[NSXMLDocument alloc]initWithData:encapsulatedData options:0 error:&error];
-          if (!xmlDocument)
-          {
-             LOG_WARNING(@"00420011 not xml\r%@",[error description]);
-             [encapsulatedData writeToFile:[sopAuditPath stringByAppendingPathComponent:@"00420011bad.xml"] atomically:NO];
-             continue;
-          }
+#pragma mark PatientID
+              //        /PatientID[^issuer]
+              //                /symlink StudyInstanceUID -> StudyInstanceUID
 
-          [[xmlDocument XMLData] writeToFile:[sopAuditPath stringByAppendingPathComponent:@"solicitud.xml"] atomically:NO];
-                 
-          //transform CDA 2 json contextualkey-values
-             
-          //serialize it to dicom
-             
-          id mwlitemjson = [xmlDocument objectByApplyingXSLT:xslt1Data
-                                                   arguments:now
-                                                       error:&error];
-          if (!mwlitemjson)
-          {
-             LOG_WARNING(@"could not transform the CDA solicitud to wlijson\r%@",[error description]);
-             continue;
+              NSString *PIDIsuffix=nil;
+              NSString *PIDI=[[[instance objectForKey:@"00100021"]objectForKey:@"Value"]firstObject];
+              if (PIDI) PIDIsuffix=[NSString stringWithFormat:@"^%@",PIDI];
+              else      PIDIsuffix=@"";
+              NSString *PID=[[[[instance objectForKey:@"00100020"]objectForKey:@"Value"]firstObject]stringByAppendingString:PIDIsuffix];
+              NSString *PIDpath=[DApath stringByAppendingPathComponent:PID];
+              if(![fileManager fileExistsAtPath:PIDpath])
+              {
+                  if(![fileManager createDirectoryAtPath:PIDpath withIntermediateDirectories:YES attributes:nil error:nil])
+                  {
+                      LOG_ERROR(@"could not create folder %@", PIDpath);
+                      continue;
+                  }
+              }
+              if (![fileManager createSymbolicLinkAtPath:[PIDpath stringByAppendingPathComponent:EUID] withDestinationPath:EUIDpath error:&error])
+              {
+                  LOG_ERROR(@"could not create PatientID symlink %@ to %@. %@",PID, EUIDpath, [error description]);
+                  continue;
+              }
           }
          
-          if (![mwlitemjson isKindOfClass:[NSData class]])
+#pragma mark download details
+          NSString *RetrieveString=[[[instance objectForKey:@"00081190"]objectForKey:@"Value"]firstObject];
+          if (RetrieveString)
           {
-             LOG_WARNING(@"ERROR xslt1 cda2mwl didn´t output data file");
-             continue;
-          }
+              NSData *downloaded=[NSData dataWithContentsOfURL:[NSURL URLWithString:RetrieveString]];
+              if (!downloaded || ![qidoResponse length])
+              {
+                  LOG_WARNING(@"NO response to %@",RetrieveString);
+                  continue;
+              }
+              
+              //unzip
+              NSError *error=nil;
+              ZZArchive *archive = [ZZArchive archiveWithData:downloaded];
+              ZZArchiveEntry *firstEntry = archive.entries[0];
+              NSData *unzipped = [firstEntry newDataWithError:&error];
+              if (error!=nil)
+              {
+                  LOG_WARNING(@"could NOT unzip %@",RetrieveString);
+                  continue;
+              }
+              
+              //get CDA: 00420011
+              NSRange range00420011=[unzipped rangeOfData:[NSData dataWithBytes:(void*)&tag00420011 length:8]
+                                                  options:0
+                                                    range:NSMakeRange(0,[unzipped length])];
+              if (range00420011.location==NSNotFound)
+              {
+                  LOG_WARNING(@"NO contiene attr 00420011: %@",RetrieveString);
+                  continue;
+              }
+              //get CDA: 00420011.length
+              uint32 capsuleLength=0x00000000;
+              [unzipped getBytes:&capsuleLength range:NSMakeRange(range00420011.location+8,4)];
+              
+              unsigned char padded=0xFF;
+              [unzipped getBytes:&padded range:NSMakeRange(range00420011.location+12+capsuleLength-1,1)];
+              NSData *encapsulatedData=[[NSData alloc]initWithData:[unzipped subdataWithRange:NSMakeRange(range00420011.location+12,capsuleLength-(padded==0))]];
+              
+              if (!encapsulatedData)
+              {
+                  LOG_WARNING(@"attr 00420011 empty: %@",RetrieveString);
+                  continue;
+              }
+              
+              NSXMLDocument *xmlDocument = [[NSXMLDocument alloc]initWithData:encapsulatedData options:0 error:&error];
+              if (!xmlDocument)
+              {
+                  LOG_WARNING(@"00420011 NOT xml %@\r%@",RetrieveString,[error description]);
+                  continue;
+              }
+#pragma mark write cda.xml
+              NSString *cdapath=[EUIDpath stringByAppendingPathComponent:@"cda.xml"];
+              [[xmlDocument XMLData] writeToFile:cdapath atomically:NO];
+              
+#pragma mark write wi.json
+              //transform CDA 2 WorkItem (wi) json contextualkey-values
+              id wi = [xmlDocument objectByApplyingXSLT:xslt1Data
+                                                       arguments:DAargdict
+                                                           error:&error];
+              if (!wi)
+              {
+                  LOG_WARNING(@"could not transform %@ to wi.json\r%@",cdapath,[error description]);
+                  continue;
+              }
+              
+              if (![wi isKindOfClass:[NSData class]])
+              {
+                  LOG_WARNING(@"xslt1 on %@ did not output data file",cdapath);
+                  continue;
+              }
 
-         //serialize it to dicom
-         NSDictionary *json=[NSJSONSerialization JSONObjectWithData:mwlitemjson options:0 error:&error];
-         if (!json)
-         {
-            LOG_WARNING(@"ERROR reading json: %@",[error description]);
-            continue;
-         }
-         NSMutableData *dicom=[NSMutableData data];
-         JD(json, dicom);
-         
-         //copy to folder DCM4CHEE
-       }
-    }
- }
- return 0;
+              NSString *wipath=[EUIDpath stringByAppendingPathComponent:@"wi.json"];
+              [wi writeToFile:wipath atomically:NO];
+
+#pragma mark serialize wi json data to dicom
+              NSDictionary *json=[NSJSONSerialization JSONObjectWithData:wi options:0 error:&error];
+              if (!json)
+              {
+                  LOG_WARNING(@"ERROR reading %@: %@",wipath,[error description]);
+                  continue;
+              }
+
+              NSMutableData *dicom=[NSMutableData data];
+              JD(json, dicom);
+              
+              [dicom writeToFile:[NSString stringWithFormat:@"/Users/Shared/wlmscpfs/aet/DCM4CHE/%@.wl",EUID] atomically:NO];
+
+          }
+     }
+  }
+  return 0;
 }
